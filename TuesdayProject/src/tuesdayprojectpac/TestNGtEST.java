bd1348c222886fef36ae2c95783f1929f08f2489
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-15 14:02:09 -0700

    class TestNGiEST.java added to project

---
+++ TuesdayProject/src/tuesdayprojectpac/TestNGtEST.java
@@ -0,0 +1,13 @@
+package tuesdayprojectpac;
+
+public class TestNGtEST {
+
+	public static void main(String[] args) {
+int a = 80;
+// here we can set the value so that any one can clone and 
+//and see it so that we are free from this all stupidity
+// The Value we have is not that perfect so you can use 
+System.out.println("value one is the First Value to deal");
+	}
+
+}
