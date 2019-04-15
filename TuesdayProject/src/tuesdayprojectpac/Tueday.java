a78398c7f1ad39f3ffb77e8d9ff7445be4957524
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-15 13:01:34 -0700

    added Method Initilize

--- TuesdayProject/src/tuesdayprojectpac/Tueday.java
+++ TuesdayProject/src/tuesdayprojectpac/Tueday.java
@@ -6,5 +6,13 @@ public class Tueday {
 		System.out.println("====YOU WILL NEVER WIN IT UNLESS YOU ARE IN IT ===");
 		// this is the value to be Aadded
 	}
+		public void Initilize(String one){
+			// here we can declare the value in the form of git 
+			one = "First Name";
+			int value  = 43;
+			
+			
+		}
+	}
 
 }
