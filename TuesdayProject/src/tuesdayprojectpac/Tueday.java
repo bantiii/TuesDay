6b9889e2f2d67c2ade24b0eb0557d19f9c00e67a
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-15 12:51:11 -0700

    added one comment

--- TuesdayProject/src/tuesdayprojectpac/Tueday.java
+++ TuesdayProject/src/tuesdayprojectpac/Tueday.java
@@ -4,6 +4,7 @@ public class Tueday {
 
 	public static void main(String[] args) {
 		System.out.println("====YOU WILL NEVER WIN IT UNLESS YOU ARE IN IT ===");
+		// this is the value to be Aadded
 	}
 
 }
