111c578ef7b53f7c3157a9a82f55a203331f99ec
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-15 12:49:34 -0700

    Added only one class Tuesday.java

---
+++ TuesdayProject/src/tuesdayprojectpac/Tueday.java
@@ -0,0 +1,9 @@
+package tuesdayprojectpac;
+
+public class Tueday {
+
+	public static void main(String[] args) {
+		System.out.println("====YOU WILL NEVER WIN IT UNLESS YOU ARE IN IT ===");
+	}
+
+}
