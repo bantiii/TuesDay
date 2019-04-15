a826c85471ba5ae119ab6a1adbefc19d63905d38
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-15 14:06:35 -0700

    I added on class JubnitProject.java

---
+++ TuesdayProject/src/tuesdayprojectpac/JubnitProject.java
@@ -0,0 +1,10 @@
+package tuesdayprojectpac;
+
+public class JubnitProject {
+
+	public static void main(String[] args) {
+//this is gonna be the first junit project
+		System.out.println("the value in junit is so cool");
+	}
+
+}
